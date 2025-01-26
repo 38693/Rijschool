9bc1b33b5ad86018364f3a493e5a9756afaf5e09
38693 <38693@ma-web.nl>
2025-01-26 20:43:07 +0100

    "Classes"

--- Rijschool/Program.cs
+++ Rijschool/Program.cs
@@ -10,41 +10,60 @@
 
     internal class Auto
     {
-
+        internal bool automaat;
+        internal string merk;
+        internal string kenteken;
+        internal int kilometerStand;
     }
 
     internal class RijLeraar
     {
-
+        internal string naam;
+        internal int leeftijd;
+        internal bool zzp;
     }
 
     internal class Student
     {
-
+        internal string naam;
+        internal int leeftijd;
+        internal LesPakket lesPakket;
+        internal RijLeraar leraar;
+        internal TheorieTest theorieTest;
+        internal RijTest rijTest;
     }
 
     internal class LesPakket
     {
-
+        internal string naam;
+        internal int leeftijd;
+        internal bool zzp;
     }
 
     internal class RijTest
     {
-
+        internal bool gehaald;
+        internal DateTime afnameDatum;
     }
 
     internal class TheorieTest
     {
-
+        internal int aantalFouten;
+        internal bool gehaald;
+        internal DateTime afnameDatum;
     }
 
     internal class Dag
     {
-
+        internal DateTime datum;
+        internal LesUur[] lesuren;
     }
 
     internal class LesUur
     {
-
+        internal int tijd;
+        internal Student student;
+        internal RijLeraar rijLeraar;
+        internal Auto auto;
     }
 }
