68211f04b3ea4da94cfe6c5c2035c883fa2506bb
38693 <38693@ma-web.nl>
2025-01-26 23:08:30 +0100

    "RijSchool"

--- Rijschool/Program.cs
+++ Rijschool/Program.cs
@@ -5,65 +5,124 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Program program = new Program();
+            program.Run();
         }
+    
+        void Run()
+        {
+
+            Auto auto1 = new Auto()
+            {
+                automaat = true,
+                kenteken = "AA824H",
+                kilometerStand = 20567,
+                merk = "volkswagen"
+            };
+
+            Auto auto2 = new Auto()
+            {
+                automaat = false,
+                kenteken = "KU786G",
+                kilometerStand = 65739,
+                merk = "volvo"
+            };
+
+            RijLeraar rijLeraar = new RijLeraar()
+            {
+                leeftijd = 30,
+                naam = "John",
+                zzp = false
+            };
+
+            LesUur lesUur = new LesUur()
+            {
+                auto = auto1,
+                rijLeraar = rijLeraar,
+
+                tijd = 1130
+            };
+
+            LesPakket lesPakket = new LesPakket
+            {
+                urenGekocht = 17,
+                urenVerbruikt = 6,
+                examenPogingen = 2,
+                automaat = true
+            };
+
+            TheorieTest theorieTest = new TheorieTest
+            {
+                aantalFouten = 4,
+                gehaald = true,
+                afnameDatum = DateTime.Now
+            };
+
+            RijTest rijTest = new RijTest
+            {
+                gehaald = false,
+                afnameDatum = DateTime.Now.AddMonths(2)
+            };
+
+            Student student1 = new Student
+            {
+                naam = "Ilias",
+                leeftijd = 16,
+                LesPakket = lesPakket,
+                RijLeraar = rijLeraar,
+                auto = auto1
+            };
+
+            LesUur lesUur1 = new LesUur
+            {
+                tijd = 14,
+                student = student1,
+                rijLeraar = rijLeraar,
+                auto = auto1
+            };
+
+            LesUur lesUur2 = new LesUur
+            {
+                tijd = 13,
+                student = student1,
+                rijLeraar = rijLeraar,
+                auto = auto2
+            };
+
+            Dag dag1 = new Dag
+            {
+                datum = DateTime.Now,
+                lesuren = new LesUur[] { lesUur1, lesUur2 }
+            };
+
+
+
+
+            Student student2 = new Student
+            {
+                naam = "Sterre",
+                leeftijd = 18,
+                LesPakket = lesPakket,
+                RijLeraar = rijLeraar,
+                TheorieTest = theorieTest,
+                RijTest = rijTest
+            };
+
+            LesUur lesUur3 = new LesUur
+            {
+                tijd = 9,
+                student = student2,
+                rijLeraar = rijLeraar,
+                auto = auto1
+            };
+
+            Dag dag2 = new Dag
+            {
+                datum = DateTime.Now.AddDays(1),
+                lesuren = new LesUur[] { lesUur3 }
+            };
+        }
+
     }
 
-    internal class Auto
-    {
-        internal bool automaat;
-        internal string merk;
-        internal string kenteken;
-        internal int kilometerStand;
-    }
-
-    internal class RijLeraar
-    {
-        internal string naam;
-        internal int leeftijd;
-        internal bool zzp;
-    }
-
-    internal class Student
-    {
-        internal string naam;
-        internal int leeftijd;
-        internal LesPakket lesPakket;
-        internal RijLeraar leraar;
-        internal TheorieTest theorieTest;
-        internal RijTest rijTest;
-    }
-
-    internal class LesPakket
-    {
-        internal string naam;
-        internal int leeftijd;
-        internal bool zzp;
-    }
-
-    internal class RijTest
-    {
-        internal bool gehaald;
-        internal DateTime afnameDatum;
-    }
-
-    internal class TheorieTest
-    {
-        internal int aantalFouten;
-        internal bool gehaald;
-        internal DateTime afnameDatum;
-    }
-
-    internal class Dag
-    {
-        internal DateTime datum;
-        internal LesUur[] lesuren;
-    }
-
-    internal class LesUur
-    {
-        internal int tijd;
-        internal Student student;
-        internal RijLeraar rijLeraar;
-        internal Auto auto;
-    }
 }
---
+++ Rijschool/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rijschool
+{
+    internal class Student
+    {
+        internal string naam;
+        internal int leeftijd;
+        internal LesPakket LesPakket;
+        internal RijLeraar RijLeraar;
+        internal TheorieTest TheorieTest;
+        internal RijTest RijTest;
+    }
+}
