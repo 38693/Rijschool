6aa848046e859c9f7d965b09438945f1d9fac6c0
38693 <38693@ma-web.nl>
2025-01-26 20:34:31 +0100

    Add project files.

---
+++ Rijschool/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Rijschool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    internal class Auto
+    {
+
+    }
+
+    internal class RijLeraar
+    {
+
+    }
+
+    internal class Student
+    {
+
+    }
+
+    internal class LesPakket
+    {
+
+    }
+
+    internal class RijTest
+    {
+
+    }
+
+    internal class TheorieTest
+    {
+
+    }
+
+    internal class Dag
+    {
+
+    }
+
+    internal class LesUur
+    {
+
+    }
+}
